347814f367426c6ed5dbf1581b44e2f1fe0e55cf
light955 <109593559+light955@users.noreply.github.com>
2025-10-20 16:26:39 +0900

    Correct button variable name and add event listener
    Fix typo in button variable name and update text on click.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// HTMLの中からidが「change-text-btn」の要素（ボタン）を探してくる
+const button = document.getElementById('change-text-btn');
+// HTMLの中からidが「main-heading」の要素（h1の見出し）を探してくる
+const heading = document.getElementById('main-heading');
 
+// ★★★ わざと綴りを間違えてみる ★★★
+buttonn.addEventListener('click', () => {
+  heading.textContent = 'JavaScriptで変更しました！( ^)o(^ )';
+});
+```    *   「Commit new file」で保存します。
