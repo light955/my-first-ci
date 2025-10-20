f4bbd5bc9272e27b5bf839244833dc351f4e2033
light955 <109593559+light955@users.noreply.github.com>
2025-10-20 16:50:21 +0900

    Correct button variable name in event listener
    Fix spelling mistake in button event listener.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// HTML要素を取得
+// HTMLの中からidが「change-text-btn」の要素（ボタン）を探してくる
 const button = document.getElementById('change-text-btn');
+// HTMLの中からidが「main-heading」の要素（h1の見出し）を探してくる
 const heading = document.getElementById('main-heading');
 
-// ★★★ ここに綴りミスのバグが残っています ★★★
-button.addEventListener('click', () => {
-  heading.textContent = 'JavaScriptで変更しました！';
+// ★★★ わざと綴りを間違えてみる ★★★
+buttonn.addEventListener('click', () => {
+  heading.textContent = 'JavaScriptで変更しました！( ^)o(^ )';
 });
+```    *   「Commit new file」で保存します。
